70c63a55b62db71bdf1020a07224def08ac28c81
sacopon <sacopon.dev@gmail.com>
2025-03-15 13:28:07 +0900

    p58

--- main.go
+++ main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"log"
 	"net/http"
+	"github.com/gorilla/mux"
 	"github.com/sacopon/apigo/handlers"
 )
 
 func main() {
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r := mux.NewRouter()
+
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 	log.Println("server start at port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
